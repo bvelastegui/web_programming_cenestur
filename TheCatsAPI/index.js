77c525899a965bdb7ba822235a99975fbef017cb
Bryan Velastegui <bryan@polimundo.com.ec>
2025-12-19 22:48:39 -0500

    Trabajo en grupo - Diseño Bootstrap

---
+++ TheCatsAPI/index.js
@@ -0,0 +1,9 @@
+async function obtenerDatos () {
+  const data = await fetch('https://api.thecatapi.com/v1/images/search')
+  const res = await data.json()
+
+  const imagen = document.getElementById('imagen')
+  imagen.src = res[0].url
+}
+
+obtenerDatos();
