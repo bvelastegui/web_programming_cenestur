63296ee80c90825b2be4771fd8a892e173ef0f4d
Bryan Velastegui <bryanslvr@gmail.com>
2025-12-21 18:51:37 -0500

    Ejemplos de arreglos básicos

---
+++ Arreglos/foreach.js
@@ -0,0 +1,7 @@
+let numeros = [7, 8, 9, 10, 25, 26, 30, 43]
+
+// Permite iterar el arreglo, no modifica el arreglo original
+// y tampoco retorna un nuevo arreglo
+numeros.forEach(numero => {
+  console.log(numero)
+});
