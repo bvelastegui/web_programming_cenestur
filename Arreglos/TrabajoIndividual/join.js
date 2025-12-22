e6b0f5556ff72cb470bf5592623f354f3c66a2d4
Bryan Velastegui <bryan@polimundo.com.ec>
2025-12-21 19:15:40 -0500

    Se resuelve el cuarto punto de la tarea individual

---
+++ Arreglos/TrabajoIndividual/join.js
@@ -0,0 +1,14 @@
+const notas = [
+  10,
+  8.90,
+  9.66,
+  6.5,
+  9.51,
+  7.8,
+  8,
+]
+
+// La función join crea y retorna un nuevo string que concatena(une) todos los
+// elementos de nuestro arreglo separado por comas
+// o por el separador que nosotros le pasamos, en este caso |
+console.log(notas.join(" | "))
