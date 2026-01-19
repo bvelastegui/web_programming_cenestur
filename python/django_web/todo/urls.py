20cdd59dcf39c7a0dbf81fabfacf68c349fd7cb1
Bryan Velastegui <bryanslvr@gmail.com>
2026-01-19 16:31:52 -0500

    Crear vista genérica `TodoListView`, plantilla base para listas y configuración de URLs en la app "todo"

---
+++ python/django_web/todo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "todo"
+urlpatterns = [
+    path('', views.TodoListView.as_view(), name='list')
+]
--- python/django_web/todo/views.py
+++ python/django_web/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
+
 
 # Create your views here.
+class TodoListView(ListView):
+    template_name = 'todo/list.html'
+    model = Task
+    context_object_name = 'tasks'
+    ordering = ['-created_at']
