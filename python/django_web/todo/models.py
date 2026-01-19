01ff101e44bd7b9c03f8685ab5f5ad15785aaf76
Bryan Velastegui <bryanslvr@gmail.com>
2026-01-19 16:31:25 -0500

    Refactorizar modelo `Task` en la app "todo": actualizar campos, nombres y tipos de datos

--- python/django_web/todo/models.py
+++ python/django_web/todo/models.py
@@ -1,3 +1,18 @@
+from datetime import timezone
+
 from django.db import models
+from django.db.models.functions import datetime
+
 
 # Create your models here.
+class Task(models.Model):
+    STATE_CHOICES = (
+        (0, 'Pendiente'),
+        (1, 'En Progreso'),
+        (2, 'Completado')
+    )
+    title = models.CharField("titulo", max_length=200)
+    description = models.TextField("descripción", null=True, blank=True)
+    status = models.IntegerField("estado", choices=STATE_CHOICES)
+    created_at = models.DateTimeField("Fecha de creación", auto_now_add=True)
+    updated_at = models.DateTimeField("Fecha de actualización", auto_now=True)
